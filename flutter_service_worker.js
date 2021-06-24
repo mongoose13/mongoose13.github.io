72e6493b6e7f8aa3c93cdd7fd6bc9e5775d660fe
Bruce Santier <banzaigm@gmail.com>
2021-06-24 10:07:58 -0700

    Add files via upload
    Upgrade to Firebase storage, with editing capability

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,17 +4,17 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "36ab8f167a5a9602083696949c2cb823",
-"index.html": "af3a26e4442b960f3e9525960a1119c2",
-"/": "af3a26e4442b960f3e9525960a1119c2",
-"main.dart.js": "ba2598ceae9e856f743b0f9f65a7c42e",
+"index.html": "1e04818ebe65ce14f55d9d5c47ff35dd",
+"/": "1e04818ebe65ce14f55d9d5c47ff35dd",
+"main.dart.js": "8be81cf3eda1ae60d240f889b8da3252",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "42380424a2643f50e7fe799775b2575d",
 "assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
-"assets/NOTICES": "1feda887e4e580e5f2ababd9daeabeac",
+"assets/NOTICES": "a54a39c677d21d1d9a19ea17c8cadbcf",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac"
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -32,7 +32,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
@@ -158,7 +158,7 @@ async function downloadOffline() {
     }
     currentContent[key] = true;
   }
-  for (var resourceKey in Object.keys(RESOURCES)) {
+  for (var resourceKey of Object.keys(RESOURCES)) {
     if (!currentContent[resourceKey]) {
       resources.push(resourceKey);
     }
